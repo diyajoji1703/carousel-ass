fda5c64a417d581c096c47b90d9e0f2f086a2378
diyajoji1703 <diyajojixyz@gmail.com>
2024-06-27 13:42:21 -0700

    first commit

---
+++ carousel-react/src/App.js
@@ -0,0 +1,67 @@
+
+
+import React from 'react';
+import { Carousel } from "react-responsive-carousel";
+
+
+
+const images = [
+  "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+  "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+];
+
+function App() {
+  return (
+    <div className="box">
+      <Carousel
+        showIndicators
+        renderArrowNext={(clickHandler, hasNext) => {
+          return (
+            hasNext && (
+              <button className="nav_btn nav_btn_right" onClick={clickHandler}>&gt</button>
+            )
+          );
+        }}
+        renderArrowPrev={(clickHandler, hasNext) => {
+          return (
+            hasNext && (
+              <button onClick={clickHandler} className="nav_btn nav_btn_left">&lt</button>
+            )
+          );
+        }}
+        renderIndicator={(clickHandler, isSelected, index) => {
+          return (
+            <li
+              onClick={clickHandler}
+              className={`ind ${isSelected ? "active" : ""}`}
+              key={index}
+              
+            />
+          );
+        }}
+        statusFormatter={(currentItem, total) => {
+          return (
+            <p>
+              Carousel
+            </p>
+          );
+        }}
+        
+        swipeable={false}        
+      >
+        {images.map((URL, index) => (
+          <div className="slide">
+            <img alt="sample_file" src={URL} key={index} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+}
+export default App;
+
+
